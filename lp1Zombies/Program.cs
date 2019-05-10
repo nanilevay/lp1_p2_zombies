c900f9a0f4abd3b56aa1fd8f2d66c0d88161e6ca
nanilev <dianuski99@gmail.com>
2019-05-10 22:12:08 +0100

    Add project files.

---
+++ lp1Zombies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lp1Zombies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
